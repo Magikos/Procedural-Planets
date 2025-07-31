ec069b21b4ad7484c4e6cdc28bd99802c6ded28f
Bryan Rodgers <magikos_99@yahoo.com>
2025-07-31 10:45:55 -0500

    Starting over.

--- Assets/Scripts/LODSettings.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Planet/LOD Settings")]
-public class LODSettings : ScriptableObject
-{
-    public int maxLOD = 5; // Levels: 0 (lowest res) to max
-    public float[] lodDistances = { 0f, 0.5f, 1f, 2f, 4f, 8f }; // Distance thresholds per level (normalize by planet radius)
-}
