e18360124d8f8b7dba300c7abfd9d9393ce368a7
Bryan Rodgers <magikos_99@yahoo.com>
2026-04-26 18:21:36 -0500

    Added determinism check: logs color/biome at fixed point each generation.

--- Assets/Scripts/Planet/Planet.cs
+++ Assets/Scripts/Planet/Planet.cs
@@ -175,6 +175,13 @@ public class Planet : MonoBehaviour
         {
             terrainFace.UpdateColors(BiomeProvider);
         }
+
+        // Determinism check: log color at a fixed point
+        var testPoint = Vector3.up;
+        float testElev = TerrainProvider.EvaluateElevation(testPoint);
+        var testBiome = BiomeProvider.EvaluateBiome(testPoint, testElev);
+        var testColor = BiomeProvider.GetBiomeColor(testPoint, testElev);
+        Logger.Log(LogLevel.Debug, "Planet", $"Determinism check: point=up elev={testElev:F6} temp={testBiome.Temperature:F4} moist={testBiome.Moisture:F4} biome={testBiome.PrimaryBiome} color=({testColor.r:F3},{testColor.g:F3},{testColor.b:F3})");
     }
 
     void GenerateWater()
